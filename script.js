f2b674a036f998db575baa3904ed75301f4fbe35
Miah111 <jrmhphifer@gmail.com>
2026-03-04 17:05:31 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+fuction calcuateQuote() {
+
+let squareFeet = parseFloat(document.getElementById("squareFeet").value);
+  let jobRate = parseFloat(document.getElementById("jobType").value);
+  let laborRate = parseFloat(document.getElementById("laborRate").value);
+  let laborHours = parseFloat(document.getElementById("laborHours").value);
+
+if (isNaN(squareFeet) || isNaN(laborRate) || isNaN(laborHours)) {
+  alert("Please fill out all fields");
+  return;
+}
+  let materialCost = squareFeet * jobRate;
+  let laborCost = laborRate * laborHours;
+
+let total = materialCost + laborCost;
+
+document.getElementById("result").innerText
+  =
+  "total: $" + total.toFixed(2);
+}
