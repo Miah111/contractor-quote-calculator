98883f07921a3db897d69ef0e501557101886bac
Miah111 <jrmhphifer@gmail.com>
2026-03-05 13:45:59 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function calculateQuote() {
   alter("Button works");
-  document.getElementById("result").innerText = "Total:$100";}
+}
 
 let squareFeet = Number(document.getElementById("squareFeet").value);
   let jobRate = Number(document.getElementById("jobType").value);
