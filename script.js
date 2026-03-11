5db0b689982d80ee281525b50680dec74e0400e7
Miah111 <jrmhphifer@gmail.com>
2026-03-11 10:04:53 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-fuction calculateQuote() {
-alter("Button works!");
+function calculateQuote() {
+  let squareFeet = parseFloat(document.getElementById("squareFeet").value);
+  let jobType = parseFloat(document.getElementById("jobType").value);
+  let laborRate = parseFloat(document.getElementById("laborRate").value); 
+  let laborHours = parseFloat(document.getElementById("laborHours).value);
+  let materials = parseFloat(document.getElementById("materials").value);
+  let profit = parseFloat(document.getElementById("profit").value);
+  let tax = parseFloat(document.getElementById("tax").value);
+
+if(isNaN(squareFeet) || isNaN(laborRate) || isNaN(laborHours)){
+  alert("Please fill in required fields");
+  return;}
+
+  let jobCost = squareFeet * jobType;
+  let laborCost = laborRate * laborHours;
+  let subtotal = jobCost + laborCost + materials;
+  let profitAmount = subtotal * (profit / 100);
+  let taxAmount = subtotal * (tax / 100);
+  let total = subtotal + profitAmount + taxAmount;
+  
+document.getElementById("result").innerHTML = "Total Quote: $" + total.toFixed(2);
 }
