f6a2e63015007b5b4e8815da5a42b0c45cf91c4a
Miah111 <jrmhphifer@gmail.com>
2026-03-13 15:49:04 -0400

    Update script.js

--- script.js
+++ script.js
@@ -17,8 +17,40 @@ if(isNaN(squareFeet) || isNaN(laborRate) || isNaN(laborHours)){
   let profitAmount = subtotal * (profit / 100);
   let taxAmount = subtotal * (tax / 100);
   let total = subtotal + profitAmount + taxAmount;
+
+  let quote = {
+    squareFeet: squareFeet,
+    jobType: jobType,
+    total: total 
+  };
+  let quotes = 
+    JSON.parse(localStorage.getItem("quotes")) ||
+    [];
+  quotes.push(quote);
+
+  localStorage.setItem("quote",
+  JSON.stringify(quotes));
+
+  displayQuotes();
   
 document.getElementById("result").innerHTML = "Total Quote: $" + total.toFixed(2);
   document.getElementById("breakdown").innerHTML = "Job Cost: $" + jobCost.toFixed(2) + "<br>" + "Labor Cost: $" + laborCost.toFixed(2) + "<br>" +
     "Materials: $" + materials.toFixed(2) + "<br>" + "Profit: $" + profitAmount.toFixed(2) + "<br>" + "Tax: $" + taxAmount.toFixed(2);
 }
+function displayQuotes() {
+  let quotes = 
+    JSON.parse(localStorage.getItem("quotes")) ||
+    [];
+  let list = 
+    document.getElementById("savedQuotes");
+  list.innerHTML = "";
+  quotes.forEach(function(q) {
+    let item = document.createElement("li");
+    
+     item.textContent = 
+     q.squareFeet + " sq ft - $" +
+     q.total.toFixed(2);
+  list.appendChild(item);
+});
+}
+displayQuotes();
